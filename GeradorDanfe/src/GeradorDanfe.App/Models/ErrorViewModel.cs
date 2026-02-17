f9b00baca91eafbc9ccbfe4b385f68f3a444582f
Tiago <Tiago@XEON-2667V4>
2026-02-17 19:44:15 -0300

    feature/add-app

---
+++ GeradorDanfe/src/GeradorDanfe.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeradorDanfe.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
