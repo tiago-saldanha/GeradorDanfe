1b95e98075676ba5bd34e259305367bd07926f6f
tiago-saldanha <tiago.avila.saldanha@gmail.com>
2026-02-23 22:09:40 -0300

    Add ads.txt for AdSense verification

--- src/GeradorDanfe.App/Controllers/HomeController.cs
+++ src/GeradorDanfe.App/Controllers/HomeController.cs
@@ -28,10 +28,20 @@ namespace GeradorDanfe.App.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
