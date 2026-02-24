242f0176ac91fe0a07c86eb030de5a7af752f2f9
tiago-saldanha <tiago.avila.saldanha@gmail.com>
2026-02-23 22:31:28 -0300

    feat: ajusta rotas e páginas para /About, /PolicyPrivacy e /Contact

--- src/GeradorDanfe.App/Controllers/HomeController.cs
+++ src/GeradorDanfe.App/Controllers/HomeController.cs
@@ -26,22 +26,7 @@ namespace GeradorDanfe.App.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult About()
-        {
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }        
+        }       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ src/GeradorDanfe.App/Controllers/PrivacyPolicyController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using GeradorDanfe.App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeradorDanfe.App.Controllers
+{
+    public class PrivacyPolicyController() : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
